70ffe592ba73615beada65481477aaed18363c4d
Janick Bergeron <janick@bergeron.com>
2021-03-15 19:38:39 +0000

    Read a PM5 characteristic

--- py/main.py
+++ py/main.py
@@ -1,34 +1,34 @@
 import asyncio
+import binascii
 import platform
+import uuid
 
-rower = None
-
+client  = None
 
 from bleak import discover
+from bleak import BleakClient, BleakScanner
+
 
 async def discoverRower():
-    global rower
     devices = await discover()
     for device in devices:
         if 'PM5' in device.name:
-            rower = device
-            return
+            return device
+    return None
 
 
-from bleak import BleakClient, BleakScanner
-
-async def print_services(device):
-    async with BleakClient(device) as client:
-        svcs = await client.get_services()
-        print("Services:")
-        for service in svcs:
-            print(type(service))
+async def poll(rower, charact):
+    async with BleakClient(rower) as client:
+        for _ in range(5): 
+            val = await client.read_gatt_char(charact)
+            print(binascii.hexlify(val))
 
             
 loop = asyncio.get_event_loop()
-loop.run_until_complete(discoverRower())
+rower = loop.run_until_complete(discoverRower())
 if rower is None:
     print("ERROR: Rower not found")
     exit
 
-loop.run_until_complete(print_services(rower))
+loop.run_until_complete(poll(rower, uuid.UUID('{ce060032-43e5-11e4-916c-0800200c9a66}')))
+print("Done.")
