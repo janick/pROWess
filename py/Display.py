b745ade98bb4e3cef4e895cb40c31c56967e63b6
Janick Bergeron <janick@bergeron.com>
2021-03-17 18:47:53 -0700

    Display updates numbers without resizing

--- py/Display.py
+++ py/Display.py
@@ -15,6 +15,7 @@
 #
 
 import tkinter as tk
+import time
 
 
 #
@@ -24,13 +25,13 @@ class NumbersFrame(tk.Frame):
     def __init__(self, parent, **kwargs):
         super().__init__(parent, kwargs)
 
-        self.WorkoutTime = tk.Label(master=self, text=" 03:10 ", font=('Arial', 40))
-        self.StrokeRate  = tk.Label(master=self, text="25", font=('Arial bold', 50))
-        self.SplitTime   = tk.Label(master=self, text=" 2:30 ", font=('Arial bold', 50))
-        self.Distance    = tk.Label(master=self, text=" 1000 m ", font=('Arial', 40))
-        self.HeartRate   = tk.Label(master=self, text="134", font=('Arial bold', 60), fg='red')
+        self.WorkoutTime = tk.Label(master=self, text=" 3:10", font=('Arial', 40), width=7)
+        self.StrokeRate  = tk.Label(master=self, text="25", font=('Arial bold', 50), width=3)
+        self.SplitTime   = tk.Label(master=self, text=" 2:30 ", font=('Arial bold', 50), width=7)
+        self.Distance    = tk.Label(master=self, text="1000 m", font=('Arial', 40), width=7)
+        self.HeartRate   = tk.Label(master=self, text="134", font=('Arial bold', 60), fg='red', width=3)
 
-        tk.Label(master=self, text="Left:", anchor="e", font=('Arial', 25)).grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
+        tk.Label(master=self, text="Left:", anchor="e", font=('Arial', 25), width=6).grid(row=0, column=0, sticky=tk.N+tk.S+tk.E+tk.W)
         self.WorkoutTime.grid(row=0, column=1, sticky=tk.N+tk.S+tk.E+tk.W)
         self.StrokeRate.grid( row=0, column=2, sticky=tk.N+tk.S+tk.E+tk.W)
         tk.Label(master=self, text="s/m", anchor="w", font=('Arial', 20)).grid(row=0, column=3, sticky=tk.N+tk.S+tk.E+tk.W)
@@ -74,11 +75,78 @@ class MainDisplay(tk.Tk):
 
         # Keep reference to the frames we need to update
         self.Numbers = bottomRightFrame
+        self.startTime = None
+        self.lastTime  = None
+        self.distance  = 0
 
+    #
+    # Start/stop workout display
+    #
+    def startWorkout(self):
+        self.startTime = int(time.time())
+        self.lastTime = self.startTime
+
+
+    def stopWorkout(self):
+        pass
+
+    #
+    # Update the data the display is tracking
+    #
+    def updateSpeed(self, speedInMeterPerSec):
+        now = int(time.time())
+
+        self.Numbers.SplitTime.configure(text=MMSS(500 / speedInMeterPerSec))
+        self.distance += speedInMeterPerSec * (now - self.lastTime)
+        self.Numbers.Distance.configure(text="{:4d} m".format(int(self.distance)))
+        self.Numbers.WorkoutTime.configure(text=MMSS(now - self.startTime))
+
+        self.lastTime = now
+        
+        
+    def updateStrokeRate(self, strokesPerMin):
+        self.Numbers.StrokeRate.configure(text=str(strokesPerMin))
+
+        
+    def updateHeartBeat(self, beatsPerMin):
+        self.Numbers.HeartRate.configure(text=str(beatsPerMin))
+
+
+#
+# Format a timevalue (in seconds) into a MM:SS string
+#
+def MMSS(seconds):
+        return "{:2d}:{:02d}".format(int(seconds/60), int(seconds)%60)
 
 #
 # Test the display layout
 #
+data = [[21, 2570, 72],
+        [18, 2570, 80],
+        [11, 2570, 88],
+        [27, 2570, 94],
+        [28, 2570, 99],
+        [25, 2570, 106],
+        [11, 2570, 133],
+        [1, 2570, 128],
+        [21, 2570, 72]]
+
+
+def test():
+    global window
+    if len(data) == 0:
+        return
+
+    window.updateStrokeRate(data[0][0])
+    window.updateSpeed(data[0][1]/100)
+    window.updateHeartBeat(data[0][2])
+
+    data.pop(0)
+    window.after(1000, test)
+
+
 window = MainDisplay(800, 1400)
+window.startWorkout()
+window.after(1000, test)
 window.mainloop()
 
