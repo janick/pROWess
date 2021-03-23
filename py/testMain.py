fd4bec0ee66f10e0860c68f15da0d09ef8cfb4e6
Janick Bergeron <janick@bergeron.com>
2021-03-22 20:10:53 -0700

    Debugged User options and created testMain

---
+++ py/testMain.py
@@ -0,0 +1,76 @@
+#
+# Copyright 2021  Janick Bergeron <janick@bergeron.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import asyncio
+import logging
+import binascii
+import json
+import os
+import platform
+import struct
+import sys
+import time
+import uuid
+
+import User
+import Workout
+import Display
+
+import aiotkinter
+
+
+
+workoutSession = None
+testMode = None
+
+User.defineUser()
+
+for arg in sys.argv:
+    if arg == "-d":
+        User.secsInOneMin  = 1
+        User.metersInOneKm = 10
+
+    if arg == "-Tt":
+        testMode = 'Time'
+    if arg == "-Td":
+        testMode = 'Distance'
+
+
+window = Display.MainDisplay(User.screenSize['X'], User.screenSize['Y'])
+workoutSession = Workout.Session(window)
+
+if testMode == "Time":
+    workoutSession.createSplits("Normal", 30, None)
+if testMode == "Distance":
+    workoutSession.createSplits("Normal", None, 5000)
+    
+interval = 0.5
+for i in range(10):
+    time.sleep(interval)
+    workoutSession.update(0, 0, 78)
+for i in range(20):
+    time.sleep(interval)
+    workoutSession.update(2, 20, 90)
+for i in range(6):
+    time.sleep(interval)
+    workoutSession.update(0, 0, 85)
+for i in range(20):
+    time.sleep(interval)
+    workoutSession.update(2, 20, 90)
+for i in range(20):
+    time.sleep(interval)
+    workoutSession.update(0, 0, 80)
+time.sleep(20)
